e3b80e7419a209ae5dcdf5bb8f7aac8a7b03322d
LucasLeandroSalvatierra <168287248+LucasLeandroSalvatierra@users.noreply.github.com>
2024-08-31 21:24:12 -0300

    Add files via upload

---
+++ sketch_240829b_tp1/sketch_240829b_tp1.js
@@ -0,0 +1,52 @@
+//variables
+let cantx=7;
+let canty=7;
+let colores;
+//imagen 
+let flechas;
+
+function preload(){
+  flechas = loadImage("data/flechas.jpeg");
+  
+}
+
+function setup() {
+  createCanvas(800,400)
+colores=color(255);
+}
+
+
+function draw() {
+background(119,3,170);
+image(flechas,0,0,400,400);
+  modY = 430 / canty;
+   modX = 400 / cantx;
+   for ( j = 0; j < canty; j++) {
+    for ( i = 0; i < cantx; i++) {
+      noStroke();
+      fill(colores);
+      rect(430 + i * modX, 50 + j * modY, 25, 15);
+      triangle(430 + i * modX, 50 + j * modY + 25, 410 + i * modX, 50 + j * modY + 7.5, 430 + i * modX, 50 + j * modY - 10);
+
+    }
+    }
+}
+function mousePressed(){
+    colores=aleatoriocolor(); // Cambia al color aleatorio
+      }
+      function aleatoriocolor(){
+ return color(random(255), random(255), random(255));
+}
+
+
+function keyPressed() {
+  if (key == 'r' || key == 'r') {
+    for ( i = 0; i < cantx; i++) {
+      for ( j = 0; j < canty; j++) {
+        colores=255;
+      }
+    }
+  }
+}
+
+  
